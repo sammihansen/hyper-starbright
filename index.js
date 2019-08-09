775c8b692dd19f5624dcd259ef56aacca9c3e994
Sam Hansen <smhansen07@gmail.com>
2019-08-08 19:14:06 -0600

    editorconfig

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cursorColor = "#98eef5";
 const foregroundColor = "#1b1b2e";
-const backgroundColor = "#fff6f6";
-const borderColor = "#fff6f6";
+const backgroundColor = "#fff";
+const borderColor = "#fff";
 const colors = {
     lightBlack: "#555c5f",
     lightRed: "#f07373",
@@ -10,15 +10,15 @@ const colors = {
     lightBlue: "#5689ad",
     lightMagenta: "#c2649f",
     lightCyan: "#4ba89d",
-    lightWhite: "#1f203a",
-    black: "#98eef5",
+    lightWhite: "#ffffff",
+    black: "#000000",
     red: "#f3c4c4",
     green: "#93ba84",
     yellow: "#feff5f",
     blue: "#bcb8ff",
     magenta: "#f3adcd",
     cyan: "#87e0dc",
-    white: "#8ba5b3"
+    white: "#fafafa"
 };
 
 exports.decorateConfig = config => {
